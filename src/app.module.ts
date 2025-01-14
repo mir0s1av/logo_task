777a74c154b4d18d05e3f2c364d8d502885061e4
Miroslav Babenko <160132080+mir0s1av@users.noreply.github.com>
2025-01-14 22:57:25 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { UploadModule } from './upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
 
+
 @Module({
   imports: [UploadModule, ConfigModule.forRoot({ isGlobal: true })],
 })
